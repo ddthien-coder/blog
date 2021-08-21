00d47d887ca61253fb04436f12178e18d5e528a0
okrabot <jungjihoonkr97@gmail.com>
2021-08-21 21:37:12 +0700

    add package antd

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <Button type='primary'>test</Button>
         </p>
         <a
           className="App-link"
