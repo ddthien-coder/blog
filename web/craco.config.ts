4abc55c3aa3ec42c192f3cf4ccaf19e6996f087b
okrabot <jungjihoonkr97@gmail.com>
2021-08-22 12:25:30 +0700

    add carco

---
+++ web/craco.config.ts
@@ -0,0 +1,18 @@
+const CracoLessPlugin = require('craco-less');
+
+module.exports = {
+  reactScriptsVersion: "react-scripts",
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { '@primary-color': '#1DA57A' },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
